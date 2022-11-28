81946079943352631911e96fc246f4658750dacb
liroynezro3@gmail.com <liroynezro3@gmail.com>
2022-11-28 17:31:38 +0200

    New commit

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -7,33 +7,40 @@ const HeroImg = () => {
   return (
     <div className={classes.hero}>
       <div className={classes.mask}>
-        <img src={Intro} className={classes.intoImg} alt={"picturae"}/>
+        <img src={Intro} className={classes.intoImg} alt={"picturae"} />
       </div>
       <div className={classes.content}>
-      <Typewriter
-  onInit={(typewriter) => {
-    typewriter.typeString('Hello everyone,')
-      .pauseFor(1800)
-      .deleteAll()
-      .typeString('My Name Is Liroy And Im Developer')
-      .pauseFor(300)
-      .deleteChars(9)
-      .typeString('<strong>Fullstack Web Developer (MERN Stack)</strong>')
-      .pauseFor(10000)
-      .start();
-  }}
-  options={{
-    autoStart: true,
-    loop: true,
-  }}/>
-      <h1>im React Development</h1>
-      <div className={classes.buttons}>
-        <Link to="/projects" className={`btn ${classes.classesbtn}`} onClick={()=>{alert("hi")}}>
-          projects
-        </Link>
-        <Link to="/contact" className="btn btn-light">
-          contact
-        </Link>
+        <Typewriter
+          onInit={(typewriter) => {
+            typewriter
+              .typeString("Hello everyone,")
+              .pauseFor(1800)
+              .deleteAll()
+              .typeString("My Name Is Liroy And Im Developer")
+              .pauseFor(300)
+              .deleteChars(9)
+              .typeString(
+                "<strong>Fullstack Web Developer (MERN Stack)</strong>"
+              )
+              .pauseFor(10000)
+              .start();
+          }}
+          options={{
+            autoStart: true,
+            loop: true,
+          }}
+        />
+        <h1>im React Development</h1>
+        <div className={classes.buttons}>
+          <Link
+            to="/projects"
+            className={`btn ${classes.classesbtn}`}
+          >
+            projects
+          </Link>
+          <Link to="/contact" className="btn btn-light">
+            contact
+          </Link>
         </div>
       </div>
     </div>
